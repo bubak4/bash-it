751edee41467029cf514d91159b98de16008a2b6
Martin Šlouf <martin@slouf.name>
2021-11-10 21:42:07 +0100

    added X resources to custom config

--- config/install.sh
+++ config/install.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Time-stamp: <2021-11-10 20:10:50 martin>
 
-#!/usr/bin/env bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+cd $SCRIPTPATH || exit 1
 
 files=".bash_profile .bashrc .bash_logout .tmux.conf bin"
 
-for i in $files ; do
+X_files="X/.xinitrc X/.xprofile  X/.Xresources  X/.xserverrc  X/.xsession"
+
+for i in $files $X_files; do
     source=$(realpath $i)
 	target="$HOME/$(basename $i)"
 	if test -L "$target" ; then

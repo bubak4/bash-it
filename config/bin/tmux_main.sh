eb31dcf036c2fd8bcc0165c3849aac9ed74f9d5d
Martin Šlouf <martin@slouf.name>
2021-11-17 21:34:42 +0100

    fix tmux setup
    - available logs are checked automatically

--- config/bin/tmux_main.sh
+++ config/bin/tmux_main.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-# Time-stamp: <2021-11-10 20:07:55 martin>
+# Time-stamp: <2021-11-17 00:56:53 martin>
 
 tmux new-session -d -s main
 
 tmux new-window -n shell -t main
 tmux new-window -n ROOT -t main 'su -'
+logs=""
+for i in messages syslog mail.info ; do
+    log=/var/log/$i
+    if test -f $log ; then
+        logs="$logs $log"
+    fi
+done
 which multitail && \
-    tmux new-window -n syslogs -t main 'sudo multitail --mergeall /var/log/messages /var/log/syslog /var/log/postgresql/postgresql-*-main.log /var/log/mail.info'
+    tmux new-window -n syslogs -t main "sudo multitail --mergeall $logs"
 which htop && \
     tmux new-window -n htop -t main 'htop'
 which s-tui && \

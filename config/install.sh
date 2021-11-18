4a3fac4c07b6465bd9dc7658b2161f31841d42b5
Martin Šlouf <martin@slouf.name>
2021-11-18 03:28:05 +0100

    improve install script, new config for i3 and vnc

--- config/install.sh
+++ config/install.sh
@@ -1,20 +1,68 @@
 #!/usr/bin/env bash
-# Time-stamp: <2021-11-10 20:10:50 martin>
+# Time-stamp: <2021-11-18 03:26:24 martin>
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH || exit 1
 
-files=".bash_profile .bashrc .bash_logout .tmux.conf bin"
-
-X_files="X/.xinitrc X/.xprofile  X/.Xresources  X/.xserverrc  X/.xsession"
-
-for i in $files $X_files; do
-    source=$(realpath $i)
-	target="$HOME/$(basename $i)"
+# $1 .. source
+function safe_link() {
+	local source=$(realpath $1)
+	local target_dir=$(realpath $HOME/$(dirname $1))
+	local target=$target_dir/$(basename $1)
 	if test -L "$target" ; then
-		echo "$target already exists"
-	else
+		echo "$target link already exists"
+	elif test -f "$target" ; then
+		mv $target $target.orig
 		ln -s $source $target
-		echo "$target -> $i link created"
+		echo "$target file replaced with link"
+	else
+		if ! test -d $target_dir ; then
+			mkdir -p $target_dir
+		fi
+		ln -s $source $target
+		echo "$target -> $source link created"
 	fi
+}
+
+#####################
+# ~/.install-secret #
+#####################
+
+echo -n "Password: "
+read secret
+if test -n "$secret" ; then
+	rm -f $SCRIPTPATH/.install-secret $HOME/.install-secret
+	echo $secret > $SCRIPTPATH/.install-secret
+	chmod 0400 $SCRIPTPATH/.install-secret
+	safe_link ".install-secret"
+else
+	cat ~/.install-secret
+fi
+
+#########
+# ~/bin #
+#########
+
+safe_link "bin"
+
+#######
+# etc #
+#######
+
+cd $SCRIPTPATH/etc
+files=$(find -type f)
+for i in $files ; do
+	safe_link $i
 done
+cd $SCRIPTPATH
+
+#########
+# etc-x #
+#########
+
+cd $SCRIPTPATH/etc-x
+files=$(find -type f | fgrep -v wallpaper)
+for i in $files ; do
+	safe_link $i
+done
+cd $SCRIPTPATH

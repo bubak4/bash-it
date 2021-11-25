8accdcdbfede713b0f02916729bb1b703fbcd689
Martin Šlouf <martin@slouf.name>
2021-11-25 08:48:29 +0100

    fix firefox cert update script

--- config/bin/update-firefox-ca.sh
+++ config/bin/update-firefox-ca.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Time-stamp: <2021-11-10 19:44:06 martin>
+# Time-stamp: <2021-11-25 08:48:20 martin>
 # synchronizes /usr/local/share/ca-certificates with firefox certstore
 # please use default system utilities to manage /usr/local/share/ca-certificates (update-ca-certificates)
 
 which certutil >/dev/null || (echo "W: please install libnss3-tools (Debian: apt install libnss3-tools)" && exit 1)
+test $? -ne 0 && exit 1
 
 firefox_ca_store=$(find ~/.mozilla -name cert9.db)
 firefox_ca_dir=$(dirname $firefox_ca_store)

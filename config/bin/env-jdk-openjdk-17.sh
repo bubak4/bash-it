9c942150cbfa8275047b5959e25e30025e818725
Martin Slouf <martin@workbox.lan>
2021-12-09 15:47:13 +0100

    env for JDK 17

---
+++ config/bin/env-jdk-openjdk-17.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Time-stamp: <2021-11-10 20:35:03 martin>
+
+version=java-17
+JAVA_HOME=$(find /usr/lib/jvm -maxdepth 1 -type d -name "*${version}*" | sort | tail -1)
+PATH=${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin:$PATH
+export JAVA_HOME PATH
+
+ANT_HOME="/usr/share/ant"
+PATH=${ANT_HOME}/bin:$PATH
+export ANT_HOME PATH
+
+MAVEN_HOME="/opt/apache/apache-maven-3.8.1"
+MAVEN_OPTS="-Xmx2048m -XX:ReservedCodeCacheSize=128m"
+PATH=${MAVEN_HOME}/bin:$PATH
+export MAVEN_HOME MAVEN_OPTS PATH

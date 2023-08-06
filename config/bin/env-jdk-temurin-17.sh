661991c271b4e2cb9dd416c7e55255770fb26e8b
Martin Šlouf <martin@slouf.name>
2023-08-06 10:33:04 +0200

    update maven for JDK's

--- config/bin/env-jdk-temurin-17.sh
+++ config/bin/env-jdk-temurin-17.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Time-stamp: <2023-01-22 19:57:45 martin>
+# Time-stamp: <2023-07-18 14:22:11 martin>
 
 version=jdk-17
 JAVA_HOME=$(find /opt/java -maxdepth 1 -type d -name "*${version}*" | sort | tail -1)
@@ -10,7 +10,7 @@ ANT_HOME="/usr/share/ant"
 PATH=${ANT_HOME}/bin:$PATH
 export ANT_HOME PATH
 
-MAVEN_HOME="/opt/apache/apache-maven-3.8.1"
+MAVEN_HOME="/opt/apache/apache-maven-3.9.3"
 MAVEN_OPTS="-Xmx2048m -XX:ReservedCodeCacheSize=512m"
 PATH=${MAVEN_HOME}/bin:$PATH
 export MAVEN_HOME MAVEN_OPTS PATH

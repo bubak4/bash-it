01f5e9b57d055efe09b280aee254404ee2ba3e16
Martin Šlouf <martin@slouf.name>
2023-10-22 13:40:13 +0200

    add JDK 21 LTS (temurin)

---
+++ config/bin/env-jdk-temurin-21.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Time-stamp: <2023-10-18 21:03:50 martin>
+
+version=temurin-21
+JAVA_HOME=$(find /usr/lib/jvm -maxdepth 1 -type d -name "*${version}*" | sort | tail -1)
+PATH=${JAVA_HOME}/bin:${JAVA_HOME}/jre/bin:$PATH
+export JAVA_HOME PATH
+
+ANT_HOME="/usr/share/ant"
+PATH=${ANT_HOME}/bin:$PATH
+export ANT_HOME PATH
+
+MAVEN_HOME="/opt/apache/apache-maven-3.9.3"
+MAVEN_OPTS="-Xmx2048m -XX:ReservedCodeCacheSize=512m"
+PATH=${MAVEN_HOME}/bin:$PATH
+export MAVEN_HOME MAVEN_OPTS PATH

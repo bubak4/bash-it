43879008b5dd3b9d7e9725d34ce018217deb3a5d
Martin Šlouf <martin@slouf.name>
2024-04-08 17:33:06 +0200

    add ls-git-add-remote utility function

--- config/bash-it/livesystems.bash
+++ config/bash-it/livesystems.bash
@@ -6,3 +6,16 @@ function ls-git-repo-setup()
 
     git config --list
 }
+
+function ls-git-add-remote()
+{
+    local remote_name=$1
+    local remote_url=$2
+
+    git remote add $remote_name $remote_url
+    git remote -v
+    echo "Press Enter to continue, Ctrl+C to exit..."
+    read NOTHING
+    git push $remote_name --all
+    git push $remote_name --tags
+}

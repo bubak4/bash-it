d93ff2d1012ff5015a06b6d9c86511e5996c599c
Martin Šlouf <martin@slouf.name>
2025-01-26 09:27:14 +0100

    prettify my send mail script

--- config/bin/mail.py
+++ config/bin/mail.py
@@ -2,33 +2,64 @@
 
 # sends simple testing mail message to the mailbox given on the command line
 
+import dataclasses
+import os.path
+import pprint
 import smtplib
 import sys
 import time
+import typing
 
-TO = list()
 FROM = "mar@centrum.cz"
 #FROM = "martin@slouf.name"
 #FROM = "martin.slouf@justlogin.cz"
 #FROM = "martin.slouf@gmail.com"
 X_RYPOUS = __file__
-SUBJECT = "test mail / zkouska mailu -- prosim neodpovidejte"
+SUBJECT = "test mail -- do not answer / zkouska mailu -- prosim neodpovidejte"
+BODY = """current time / aktualni cas: %s
+sent by postmaster / odeslano spravcem mailu
+""" % (time.strftime("%Y-%m-%d %H:%M:%S"))
 
-if (__name__ == "__main__"):
+@dataclasses.dataclass
+class Mail:
+    sender: str
+    recipients: typing.List[str]
+    subject: str
+    body: typing.Optional[str]
+    custom_headers: typing.Optional[typing.Dict[str, str]]
+
+    def create_message(self):
+        headers = {}
+        headers["From"] = self.sender
+        headers["To"] = ", ".join(self.recipients)
+        headers["Subject"] = self.subject
+        headers.update(self.custom_headers)
+
+        msg = "\r\n".join(["%s: %s" % (k, v) for (k, v) in headers.items()])
+        msg += "\r\n\r\n"
+        msg += self.body
+
+        return msg
+
+    def send_via_localhost(self):
+        msg = self.create_message()
+        server = smtplib.SMTP('localhost')
+        try:
+            server.sendmail(self.sender, self.recipients, msg)
+        finally:
+            server.quit()
+
+def usage():
+    name = os.path.basename(sys.argv[0])
+    print("Usage: %s <recipient>" % (name))
+
+if (__name__ == "__main__"):    
     if (len(sys.argv) != 2):
-        print("E: wrong arguments")
+        usage()
         sys.exit(2)
     else:
-        TO.append(sys.argv[1])
-    msg = "From: %s\r\nTo: %s\r\nX-Rypous: %s\r\nSubject: %s\r\n\r\n" % \
-          (FROM, ", ".join(TO), X_RYPOUS, SUBJECT)
-    msg += "current timestamp: %d\r\n" % (time.time())
-    msg += "odeslano spravcem mailu\r\n"
-    print(msg)
-    server = smtplib.SMTP('localhost')
-    try:
-        server.sendmail(FROM, TO, msg)
-    except:
-        pass
-    finally:
-        server.quit()
+        recipient = sys.argv[1]
+
+    mail = Mail(FROM, [recipient], SUBJECT, BODY, {"X-Rypous": X_RYPOUS})
+    pprint.pprint(mail, width=120)
+    mail.send_via_localhost()

3653d0ee8c1ec10992a1ae55232748b7f34892a3
Martin Šlouf <martin@slouf.name>
2025-12-12 11:03:04 +0100

    modernize dirsize

--- config/bash-it/dirsize.bash
+++ config/bash-it/dirsize.bash
@@ -3,18 +3,18 @@
 # prints out directories > 1M (only 1-level subdirs)
 function dirsize()
 {
-    directory=.
+    directory=$(pwd)
     if test -d "$1"; then
         directory=$1
     fi
 
-    cd $directory
-    echo "`date  +'%X %x'` "
+    cd "$directory" || return
+    date --iso-8601=seconds
 
     du -h 2>/dev/null \
         | sort -h \
         | sed -e "s/\.\///g" \
         | egrep -e "([0-9]+(,[0-9]+)?)[MG][[:space:]]+" \
         | fgrep -v -e "/"
-    cd - 1>/dev/null
+    cd - 1>/dev/null || return
 }

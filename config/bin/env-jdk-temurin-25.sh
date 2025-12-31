766960b03a7133a6992f4b8f2a513f71a33482ff
Martin Šlouf <martin@slouf.name>
2025-12-31 12:34:30 +0100

    update maven to 3.9.12

--- config/bin/env-jdk-temurin-25.sh
+++ config/bin/env-jdk-temurin-25.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Time-stamp: <2025-12-05 19:11:41 martin>
+# Time-stamp: <2025-12-18 13:16:38 martin>
 
 version=temurin-25
 JAVA_HOME=$(find /usr/lib/jvm -maxdepth 1 -type d -name "*${version}*" | sort | tail -1)
@@ -10,7 +10,7 @@ ANT_HOME="/usr/share/ant"
 PATH=${ANT_HOME}/bin:$PATH
 export ANT_HOME PATH
 
-MAVEN_HOME="/opt/apache/apache-maven-3.9.11"
+MAVEN_HOME="/opt/apache/apache-maven-3.9.12"
 MAVEN_OPTS="-Xmx2048m -XX:ReservedCodeCacheSize=512m"
 PATH=${MAVEN_HOME}/bin:$PATH
 export MAVEN_HOME MAVEN_OPTS PATH

d28dddb8654123695bb47c3b400a871e772ffe56
Martin Šlouf <martin@slouf.name>
2026-07-14 13:33:40 +0200

    mise tool setup

---
+++ config/bash-it/mise.bash
@@ -0,0 +1,14 @@
+# mise for k8s tooling management
+# curl https://mise.run | sh
+
+if command -v mise > /dev/null ; then
+    eval "$(~/.local/bin/mise activate bash)"
+fi
+
+# list available versions
+#mise ls-remote kubectl
+
+# install (latest version) globally
+#mise use -g kubectl
+# install specific version
+#mise use -g kubectl@1.36.2
